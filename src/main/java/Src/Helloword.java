27e82f413ad21398cc4525b64d00bac1c2847e88
liangxin <xiaofengguboy@163.com>
2021-06-03 16:35:14 +0800

    添加了for循环

--- src/main/java/Src/Helloword.java
+++ src/main/java/Src/Helloword.java
@@ -4,6 +4,10 @@ public class Helloword {
     public static void main(String[] args) {
         System.out.println("hello word");
         System.out.println("我是程序b");
+        for (int i= 0; i <=10; i++) {
+
+            System.out.println(i);
+        }
 
     }
 }
